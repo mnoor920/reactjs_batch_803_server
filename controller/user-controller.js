2090910a90925d7a4dfc03899581cf4627fc7eb1
mnoor920 <mnoor92081@hotmail.com>
2024-02-06 22:43:47 +0500

    cors updated

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,23 +1,32 @@
 import User from "../model/user_model.js";
 
-
 export const SignUp = async (request, response) => {
-    let userMatch = await User.findOne({ email: request.body.email })
-    if (!userMatch) {
-        try {
-            const newUser = new User({
-                first_name: request.body.first_name,
-                last_name: request.body.last_name,
-                email: request.body.email,
-                password: request.body.password,
-            })
-            await newUser.save()
-            response.status(200).json({ msg: 'User Sign Up Successfully' })
-        } catch (error) {
-            response.status(500).json(error)
+    try {
+        if (!request.body) {
+            return response.status(400).json({ error: "Request body is missing or empty" });
         }
+        const { first_name, last_name, email, password } = request.body;
+
+        if (!first_name || !last_name || !email || !password) {
+            return response.status(400).json({ error: "Missing required fields in the request body" });
+        }
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return response.status(400).json({ msg: "User email already exists" });
+        }
+        const newUser = new User({
+            first_name,
+            last_name,
+            email,
+            password
+        });
+        await newUser.save();
+
+        // Return success message
+        return response.status(200).json({ msg: 'User signed up successfully' });
+    } catch (error) {
+        // Log and handle errors
+        console.error("Error signing up user:", error);
+        return response.status(500).json({ error: "An error occurred while signing up user" });
     }
-    else {
-        return response.status(400).json({ msg: "User email already Exist" })
-    }
-}
+};
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import Connection from './db/database.js'
 import Router from './routes/route.js'
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', Router)
 
